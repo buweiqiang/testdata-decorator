7984be40ef3b0d26225c59ce6391def04854e069
buweiqiang <buweiqiang@mistong.com>
2018-09-07 18:12:11 +0800

    add parent folder in sys.path to make sample easy to run

--- testdata_usage/sample_test.py
+++ testdata_usage/sample_test.py
@@ -1,6 +1,10 @@
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+
 import unittest
 import testdata
-
 
 class testcls1(unittest.TestCase):
     def setUp(self):
